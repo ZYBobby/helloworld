4a037659e8f578e8e041bb655720a3228c040b05
Bobby <Bobbu@bobbybydeMacBook-Pro.local>
2017-05-29 20:48:14 +0800

    my cpp

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+  long long num;
+  cin>>num;
+
+  if(num%2==1)
+     cout<<"odd"<<endl;
+  else
+     cout<<"even"<<endl;
+  return 0;
+}
---
+++ Untitled3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int r;
+    cin>>r;
+    cout<<2*r*3.14<<"Circumference"<<endl;
+    cout<<r*r*3.14<<"area of a circle"<<endl;
+    return 0;
+}
