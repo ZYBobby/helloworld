32c4d6a0c9a5dc20e6383d5bee7b03f5f874ff46
Bobby <Bobbu@bobbybydeMacBook-Pro.local>
2017-06-04 18:29:36 +0800

    输入姓名并问好--字串应用

---
+++ Untitled6.cpp
@@ -0,0 +1,13 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"What's your name ^_^?\nMy name is ";
+
+     cin>>name;
+    cout<<"Hello, "<<name<<"."<<endl;
+    return 0;
+}
