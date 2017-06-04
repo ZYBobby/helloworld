b69077f3645ccf32414c4b7a13d0e66ab70daf57
ZYBobby <1085490997@qq.com>
2017-06-04 17:47:10 +0800

    Add files via upload
    这是一个小游戏程序，本小程序员会不断改进，出2.0版本哟。^_^

---
+++ Untitled5.cpp
@@ -0,0 +1,16 @@
+//Ìî×ÖÄ¸ÓÎÏ·1.0
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+   cout<<"Letter filling:B_bby"<<endl;
+   char a;
+   cin>>a;
+   cout<<"B"<<a<<"bby"<<endl;
+   if(a=='o')
+    cout<<"correct!"<<endl;
+   else
+    cout<<"negative..."<<"Bobby!"<<"Not"<<" B"<<a<<"bby"<<endl;
+   return 0;
+}
