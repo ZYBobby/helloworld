1443435183f16758735a71f30e43e1c0446e32a7
ZYBobby <1085490997@qq.com>
2017-07-09 08:48:29 +0800

    Add files via upload

---
+++ my10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    float x;
+    float y;
+    x=sqrt(5290.0/4000.0)-1;
+    y=5290*(1+x);
+    cout<<y<<endl;
+    return 0;
+}
