66f1c20dbd971aceb8c39c8a602686ff4b056bf5
ZYBobby <1085490997@qq.com>
2017-07-14 18:55:19 +0800

    Add files via upload

---
+++ my15.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    long long a,b,c;
+    long long s;
+    a=1010101010101.0;
+    b=101010101010.0;
+    c=1111111111111.0;
+    s=a+b+c;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c="<<c<<endl;
+    cout<<"s=a+b+c"<<endl;
+    cout<<"s="<<s<<endl;
+    return 0;
+}
